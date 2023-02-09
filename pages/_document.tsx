03d9c6754d480c675f7cff7b4d8899cf42cb442e
Donald W. Boulton <donaldboulton@gmail.com>
2023-02-09 09:14:06 -0600

    gtm cokiehub

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,27 +15,43 @@
  */
 
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+               })(window,document,'script','dataLayer','GTM-PXRSPG6');`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied', 'wait_for_update': 500});
+                var cpm = {};
+                (function(h,u,b){
+                    var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
+                    e.async=true;e.src='https://cookiehub.net/c2/6b9c3004.js';
+                    e.onload=function(){u.cookiehub.load(b);};
+                    d.parentNode.insertBefore(e,d);
+                })(document,window,cpm);`,
+            }}
+          />
+        </Head>
         <body className="loading">
           <Main />
           <NextScript />
-          <Script
-            src="cookiehub.js"
-            strategy="afterInteractive"
-          />
-          <Script
-            src="gtm.js"
-            strategy="afterInteractive"
-          />
-          <Script
-            src="noscript.js"
-            strategy="afterInteractive"
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXRSPG6" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
           />
         </body>
       </Html>
