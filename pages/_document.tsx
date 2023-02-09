6dc90944f91284d9ce850d1df6e66b3c9d616f25
Donald W. Boulton <donaldboulton@gmail.com>
2023-02-09 03:18:03 -0600

    ts particles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,19 +22,20 @@ import type { Container, Engine } from 'tsparticles-engine';
 import { loadStarsPreset } from 'tsparticles-preset-stars';
 
 export default class CustomDocument extends Document {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadStarsPreset(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container: Container | undefined) => {
-      await console.log(container);
-  }, []);
+  
   render() {
+    const particlesInit = useCallback(async (engine: Engine) => {
+      console.log(engine);
+  
+      // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      await loadStarsPreset(engine);
+    }, []);
+  
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
     const options = {
       preset: "stars",
     };
@@ -57,7 +58,7 @@ export default class CustomDocument extends Document {
             strategy="afterInteractive"
           />
           {/* @ts-ignore */}
-          <Particles id="tsparticles" init={this.particlesInit} loaded={this.particlesLoaded} options={options} />
+          <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={options} />
         </body>
       </Html>
     );
