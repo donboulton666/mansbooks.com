f3c743a7e6936354245277be816d70a827d930eb
Donald W. Boulton <donaldboulton@gmail.com>
2023-02-09 18:28:42 -0600

    Sponsor

--- components/speaker-section.tsx
+++ components/speaker-section.tsx
@@ -18,7 +18,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import cn from 'classnames';
 import GithubIcon from '@components/icons/icon-github';
-import { Speaker, Sponsor } from '@lib/types';
+import { Speaker } from '@lib/types';
+import { Sponsor } from '@lib/types';
 import styles from './speaker-section.module.css';
 import styleUtils from './utils.module.css';
 
@@ -34,6 +35,9 @@ const TwitterIcon = () => (
 
 type Props = {
   speaker: Speaker;
+};
+
+type Props = {
   sponsor: Sponsor;
 };
 
