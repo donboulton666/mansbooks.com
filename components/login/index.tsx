d7ac44bab97b75820057fb55d7fbb1a4eabf9580
Donald W. Boulton <donaldboulton@gmail.com>
2023-02-11 16:49:21 -0600

    modal rename

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -2,22 +2,22 @@ import { FADE_IN_ANIMATION_SETTINGS } from "../../lib/constants";
 import { AnimatePresence, motion } from "framer-motion";
 import { signIn, useSession } from "next-auth/react"
 import { PersonIcon } from '@100mslive/react-icons';
-import { useSignInModal } from "./sign-in-modal";
+import { Modal, useModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 
 export default function Modal() {
   const { data: session, status } = useSession();
-  const { setShowSignInModal } = useSignInModal();
+  const { setShownModal } = useModal();
 
   return (
     <>
-      <SignInModal />
+      <Modal />
       <div>
         <AnimatePresence>
           {!session && status !== "loading" ? (            
             <motion.button
               className="rounded-lg border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-              onClick={() => setShowSignInModal(true)}
+              onClick={() => setShowModal(true)}
               {...FADE_IN_ANIMATION_SETTINGS}
             >
               <PersonIcon />
