6467a3bd0a3a972f6581f94a0c7e71155410604b
Donald W. Boulton <donaldboulton@gmail.com>
2023-03-01 20:28:45 -0600

    add Google Calendar

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -1,23 +1,8 @@
-/**
- * Copyright 2020 Vercel Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import cn from "classnames";
 import { Stage, Talk } from "@lib/types";
 import styles from "./schedule.module.css";
 import TalkCard from "./talk-card";
+import GoogleCalendar from "@components/GoogleCalendar"
 
 function StageRow({ stage }: { stage: Stage }) {
   // Group talks by the time block
@@ -58,6 +43,7 @@ export default function Schedule({ allStages }: Props) {
           <StageRow key={stage.slug} stage={stage} />
         ))}
       </div>
+      <GoogleCalendar />
     </div>
   );
 }
