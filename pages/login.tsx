c6ffbb739c4e97ff91f5fa7252f47ddebc98e9d9
Donald W. Boulton <donaldboulton@gmail.com>
2023-04-07 18:35:24 -0500

    Supabase integration auth and backend

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,7 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import Account from '@components/Account'
+import Account from '@components/account'
 import Layout from "@components/PageLayout"
 
 const Login = () => {
