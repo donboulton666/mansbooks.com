d526993e17f922bef9739e73c91ece31daf5c157
Donald W. Boulton <donaldboulton@gmail.com>
2023-07-31 19:31:29 -0500

    try emoji-mart picker

--- components/Chat/ChatContainer.tsx
+++ components/Chat/ChatContainer.tsx
@@ -2,24 +2,24 @@ import {
   useHMSStore,
   selectHMSMessages,
   useHMSActions,
-} from '@100mslive/hms-video-react';
-import React from 'react';
-import { useEffect, useState } from 'react';
-import ChatFeed from './ChatFeed';
-import ChatInput from './ChatInput';
-import ChatLayout from './ChatLayout';
-import Chat from './Chat';
+} from "@100mslive/hms-video-react";
+import React from "react";
+import { useEffect, useState } from "react";
+import ChatFeed from "./ChatFeed";
+import ChatInput from "./ChatInput";
+import ChatLayout from "./ChatLayout";
+import Chat from "./Chat";
 
 const ChatContainer = () => {
   const hmsActions = useHMSActions();
   const storeMessages = useHMSStore(selectHMSMessages);
-  const [chatInput, setChatInput] = useState('');
+  const [chatInput, setChatInput] = useState("");
   const sendMessage = () => {
     hmsActions.sendBroadcastMessage(chatInput);
-    setChatInput('');
+    setChatInput("");
   };
   useEffect(() => {
-    const el = document.getElementById('chat-feed');
+    const el = document.getElementById("chat-feed");
     if (el) {
       el.scrollTop = el.scrollHeight;
     }
--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import InputEmoji from 'react-input-emoji'
+import React from "react";
+import { useState } from "react";
+import { Picker } from "emoji-mart";
 
 const ChatInput = ({ value, onChange, onKeyPress }) => {
+  const [showEmojis, setShowEmojis] = useState(false);
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
   return (
-    <InputEmoji
-      value={value}
-      onChange={onChange}
-      type='text'
-      className='px-3 py-2 m-2 bg-slate-900 focus:bg-black rounded-3xl border-none focus:ring-1 ring-brand-100 outline-none placeholder-slate-200 w-72'
-      placeholder='Send Message'
-      onKeyPress={(event) => {
-        if (event.key === 'Enter') {
-          if (!event.shiftKey) {
-            event.preventDefault();
-            if (value.trim() !== '') {
-              onKeyPress();
+    <div className="items-center justify-center">
+      <input
+        value={value}
+        onChange={onChange}
+        type="text"
+        className="m-2 w-72 rounded-3xl border-none bg-slate-900 px-3 py-2 placeholder-slate-200 outline-none ring-brand-100 focus:bg-black focus:ring-1"
+        placeholder="Send Message"
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            if (!event.shiftKey) {
+              event.preventDefault();
+              if (value.trim() !== "") {
+                onKeyPress();
+              }
             }
           }
-        }
-      }}
-    />
+        }}
+      />
+      <button
+        className="border-0 bg-transparent outline-0"
+        onClick={() => setShowEmojis(!showEmojis)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+      {showEmojis && (
+        <>
+          <Picker onSelect={addEmoji} theme={dark} set={apple} />
+        </>
+      )}
+    </div>
   );
 };
 
