094aaa40745021c49c7c92ba5a36557c13b0053a
Donald W. Boulton <donaldboulton@gmail.com>
2023-08-24 17:09:29 -0500

    youtube data

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,9 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
 
-import type { NextRequest } from "next/server";
-
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  // if user is signed in and the current path is / redirect the user to /account
-  if (user && req.nextUrl.pathname === "/login") {
-    return NextResponse.redirect(new URL("/user-account", req.url));
-  }
-
-  // if user is not signed in and the current path is not / redirect the user to /
-  if (!user && req.nextUrl.pathname !== "/user-account") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  return res;
+export async function middleware(req) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  await supabase.auth.getSession()
+  return res
 }
-
-export const config = {
-  matcher: ["/", "/user-account"],
-};
