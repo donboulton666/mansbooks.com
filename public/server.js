2f4fab456fd280970dce6f685e2d37614bc9e255
Donald W. Boulton <donaldboulton@gmail.com>
2023-09-05 14:19:28 -0500

    about 80% done

--- public/server.js
+++ public/server.js
@@ -10,16 +10,19 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname } = parsedUrl
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
 
-    if (pathname === '/sw.js' || /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)) {
-      const filePath = join(__dirname, '.next', pathname)
-      app.serveStatic(req, res, filePath)
+    if (
+      pathname === "/sw.js" ||
+      /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)
+    ) {
+      const filePath = join(__dirname, ".next", pathname);
+      app.serveStatic(req, res, filePath);
     } else {
-      handle(req, res, parsedUrl)
+      handle(req, res, parsedUrl);
     }
   }).listen(3000, () => {
-    console.log(`> Ready on http://localhost:${3000}`)
-  })
-})
+    console.log(`> Ready on http://localhost:${3000}`);
+  });
+});
